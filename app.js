6d2f4bac1f4f8f2fd86982a233472709640f106f
Bl00mstar <mjakstefan02@gmail.com>
2021-01-22 18:27:10 +0100

    Setup

---
+++ app.js
@@ -0,0 +1,6 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
